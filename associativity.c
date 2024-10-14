c7ec42b141f207bdf19119fec51cb64f9e1c8512
Solene.Lemonnier <solene.lemonnier@insa-rennes.fr>
2024-10-14 10:46:09 +0200

    refactor file

--- associativity.c
+++ associativity.c
@@ -9,15 +9,6 @@ void aleatoire(double *x, double *y, double *z) {
     *z = ((double)rand() / RAND_MAX) * 200 - 100;
 }
 
-void chiffresUtilisateurs(double *x, double *y, double *z) {
-    // Demander à l'utilisateur de saisir trois chiffres
-    printf("Entrez le premier chiffre : ");
-    scanf("%lf", x);
-    printf("Entrez le deuxième chiffre : ");
-    scanf("%lf", y);
-    printf("Entrez le troisième chiffre : ");
-    scanf("%lf", z);
-}
 
 void calculerTauxAssociativite(int totalExperiences) {
     double x, y, z;
@@ -51,47 +42,9 @@ int main() {
     // Initialiser le générateur de nombres aléatoires
     srand(time(NULL));
 
-    // Boucle jusqu'à ce que l'utilisateur entre 1 ou 2
-    while (version != 1 && version != 2 && version!=3 ) {
-        printf("Voulez-vous tester l'associativité en aléatoire (1), entrer vos propres nombres (2) ou calculer un taux d'associativité (3) ? ");
-        scanf("%d", &version);
+    int totalExperiences=50000;
 
-        if (version != 1 && version != 2 && version != 3) {
-            printf("Option invalide, veuillez choisir 1, 2 ou 3.\n");
-        }
-    }
-  
-    if (version == 1) {
-        aleatoire(&x, &y, &z);
-        printf("Chiffres aléatoires générés : x=%.2f, y=%.2f, z=%.2f\n", x, y, z);
-    } else if (version == 2) {
-        chiffresUtilisateurs(&x, &y, &z);
-    } else if (version == 3) {
-        int totalExperiences;
-
-        printf("Combien de fois voulez-vous exécuter l'expérience ? ");
-        scanf("%d", &totalExperiences);
-        
-        // Assurez-vous que le nombre d'expériences est positif
-        if (totalExperiences > 0) {
-            calculerTauxAssociativite(totalExperiences);
-        } else {
-            printf("Le nombre d'expériences doit être positif.\n");
-        }
-    }
-
-    // Calculer les résultats pour les options 1 et 2
-    if (version == 1 || version == 2) {
-        double res1 = (x + y) + z;
-        double res2 = x + (y + z);
-
-        // Vérifier l'associativité
-        if (res1 == res2) {
-            printf("L'associativité est vérifiée : (x+y)+z == x+(y+z) = %.2f\n", res1);
-        } else {
-            printf("L'associativité n'est pas vérifiée : res1 = %.2f et res2 = %.2f\n", res1, res2);
-        }
-    }
-
+    calculerTauxAssociativite(totalExperiences);
+    
     return 0;
 }
